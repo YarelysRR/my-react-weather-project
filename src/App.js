61cc47885d73f08d5e76fdb96302f0f328811921
Yarelys Rivera <yarelysrr@gmail.com>
2021-12-05 20:29:35 -0800

    added Github repo url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App </div>;
+  return (
+    <div className="App">
+      <h1>Weather App </h1>
+      <footer>
+        This project was created by Yarelys Rivera, hosted on Netlify and is{" "}
+        <a
+          href="https://github.com/YarelysRR/my-react-weather-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced
+        </a>{" "}
+      </footer>
+    </div>
+  );
 }
