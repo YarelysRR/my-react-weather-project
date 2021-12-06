a8b1a82721485e61c134fb9b653abce363a00be0
Yarelys Rivera <yarelysrr@gmail.com>
2021-12-05 20:46:43 -0800

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App </h1>
-      <footer>
-        This project was created by Yarelys Rivera, hosted on Netlify and is{" "}
-        <a
-          href="https://github.com/YarelysRR/my-react-weather-project"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced
-        </a>{" "}
-      </footer>
+      <div className="container">
+        <h1>Weather App </h1>
+        <footer>
+          This project was created by Yarelys Rivera, hosted on Netlify and is{" "}
+          <a
+            href="https://github.com/YarelysRR/my-react-weather-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced
+          </a>{" "}
+        </footer>
+      </div>
     </div>
   );
 }
