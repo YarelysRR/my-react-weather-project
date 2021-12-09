8c6c8d36da081ade161956f46410a420cc2e95f3
Yarelys Rivera <yarelysrr@gmail.com>
2021-12-08 21:16:27 -0800

    Changed API key

--- src/Weather.js
+++ src/Weather.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import FormattedDate from "./FormattedDate";
 import axios from "axios";
 import "./Weather.css";
 
@@ -14,7 +15,7 @@ export default function Weather(props) {
       humidity: response.data.main.humidity,
       wind: response.data.wind.speed,
       iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
-      date: "Wednesday, 8:30pm",
+      date: new Date(response.data.dt * 1000),
     });
   }
   if (weatherData.ready) {
@@ -42,7 +43,9 @@ export default function Weather(props) {
         <h1> {weatherData.city} </h1>
         <ul>
           {" "}
-          <li> {weatherData.date}</li>
+          <li>
+            <FormattedDate date={weatherData.date} />
+          </li>
           <li className="text-capitalize"> {weatherData.description}</li>{" "}
         </ul>
         <div className="row mt-3">
@@ -74,7 +77,7 @@ export default function Weather(props) {
       </div>
     );
   } else {
-    const apiKey = "3c325847e00065609ab37a2eb6d3ff85";
+    const apiKey = "8d3b4eb3bfd4da849a5a61c1e36fe700";
     let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${props.defaultcity}&appid=${apiKey}&units=imperial`;
 
     axios.get(apiUrl).then(handleRespose);
