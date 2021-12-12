82d31594bcf49440270122d5eee80b3e6ba794b2
Yarelys Rivera <yarelysrr@gmail.com>
2021-12-11 22:41:22 -0800

    created new component , units

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemp from "./WeatherTemp";
 
 export default function WeatherInfo(props) {
   return (
@@ -20,12 +21,7 @@ export default function WeatherInfo(props) {
             </div>
           </div>
           <div className="float-left">
-            <span className="temperature">
-              {" "}
-              {Math.round(props.data.temperature)}{" "}
-            </span>{" "}
-            <span className="unit">°F | </span>{" "}
-            <span className="unit">°C </span>
+            <WeatherTemp fahrenheit={props.data.temperature} />
           </div>
         </div>
         <div className="col-6">
