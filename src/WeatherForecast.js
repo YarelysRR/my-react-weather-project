28c1b4d1300f4058404806ec1a7783bf74314211
Yarelys Rivera <yarelysrr@gmail.com>
2021-12-13 18:39:27 -0800

    Created WeatherForecastDay

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,33 +1,36 @@
-import React from "react";
-import WeatherIcon from "./WeatherIcon";
+import React, { useState } from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
+import WeatherForecastDay from "./WeatherForecastDay";
 
-export default function WeatherForescast(props) {
+export default function WeatherForecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+
   function handleResponse(response) {
-    console.log(response.data);
+    setForecast(response.data.daily);
+    setLoaded(true);
   }
 
-  let apiKey = "8d3b4eb3bfd4da849a5a61c1e36fe700";
-  let longitude = props.coordinates.lon;
-  let latitude = props.coordinates.lat;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
-  axios.get(apiUrl).then(handleResponse);
-
-  //also had to include info in the weather.js  setWeatherData now includes coord and also down in the component it has to include the coordinates.
-
-  return (
-    <div className="WeatherForecast">
-      <div clasName="row">
-        <div className="col">
-          <div className="WeatherForDay">THU</div>{" "}
-          <WeatherIcon code="01n" size={36} />
-          <div className="WeatherForeTemp">
-            <span className="WeatherForTempMax">35°</span>
-            <span className="WeatherForTempMin">3°</span>
+  if (loaded) {
+    console.log(forecast);
+    return (
+      <div className="WeatherForecast">
+        <div clasName="row">
+          <div className="col">
+            <WeatherForecastDay data={forecast[0]} />
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apiKey = "8d3b4eb3bfd4da849a5a61c1e36fe700";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
+    axios.get(apiUrl).then(handleResponse);
+    ///also had to include info in the weather.js  setWeatherData now includes coord and also down in the component it has to include the coordinates.
+
+    return null;
+  }
 }
